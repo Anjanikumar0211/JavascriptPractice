8478bda9ea2bc64335abf29fa30e6f815be30ced
ANJANI KUMAR <87645443+Anjanikumar0211@users.noreply.github.com>
2023-08-22 11:59:06 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,9 +1,21 @@
 let score = 33
 
 let booleanIsLoggedIn = Boolean(score)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 = true ; 0=> false
 // "" => false
 // "Rohit" => ture
 // 33 => true
+
+
+//************** Operation*************
+let value = 3
+let negValue = -value
+//console.log(negValue);
+//console.log(2+2);
+let str = "Hello "
+let str2 = "rohit"
+
+//console.log(str + str2)
+console.log("1" +( 2 + 2));
