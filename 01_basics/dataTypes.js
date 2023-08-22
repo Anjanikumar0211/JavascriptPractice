5eb089c96b729c9259a1dc6d83458c8719973428
ANJANI KUMAR <87645443+Anjanikumar0211@users.noreply.github.com>
2023-08-22 11:25:49 +0000

    datatype

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,14 @@
+let name = "Anjani kumar"
+let age = 24
+let isLoggedIn = true
+
+console.log(typeof null);
+console.log(name, age); 
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => standalone value (true ,False)
+//undefined => undefined
+//null => object
+//symbol => unique
